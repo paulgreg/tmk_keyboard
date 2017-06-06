fe7720efa956a9be99dca1a4668e18fb3849cae9
Grégory Paul <paulgreg@gmail.com>
2017-06-06 20:48:15 +0200

    Remove commented line

--- keyboard/3keys/keymap_poker.c
+++ keyboard/3keys/keymap_poker.c
@@ -11,5 +11,4 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 const action_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_TOGGLE(0)
-        //ACTION_DEFAULT_LAYER_SET(0),
 };
